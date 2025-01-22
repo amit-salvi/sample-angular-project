bd39805e6b7d561912dd48eaf807bf0b43a483a2
amit.salvi16@gmail.com <G!tHbb@2024>
2025-01-22 01:31:32 +0000

    angular sample project showing nav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+    { path: '', component: HomeComponent }, // Default route
+    { path: 'about', component: AboutComponent }, // About page
+    { path: 'contact', component: ContactComponent }, // Contact page
+    { path: '**', redirectTo: '', pathMatch: 'full' }, // Redirect unknown paths to Home
+  ];
+  
